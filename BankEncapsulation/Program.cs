d1064f1a19a3bd29dfa148433344de88fee2872b
Ahmad Stanikzai <Shekibstanikzai.2016@gmail.com>
2022-12-09 08:00:59 -0800

    done

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much Money would you like to Deposit");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance, 0:c}");
         }
     }
 }
